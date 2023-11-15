bb4838fca08fe76cd7472b66004cb1b1ab61c829
Mulew <muluken1393@gmail.com>
2023-11-15 13:23:15 +0300

    establsihe mongoose connection

--- API/config.js
+++ API/config.js
@@ -1,4 +1,7 @@
  const  PORT = 8080;
+const MongoDBURL = "mongodb://127.0.0.1:27017/BookStore";
 
-
- module.exports = PORT;
+ module.exports = {
+    PORT,
+    MongoDBURL
+ }
--- API/index.js
+++ API/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
-const {PORT} = require('./config');
-
+const { PORT, MongoDBURL } = require('./config');
+const mongoose = require('mongoose');
 const app = express();
 
+mongoose.connect(MongoDBURL)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.log('Error connecting to MongoDB:', err);
+  });
 
-
-app.listen((PORT),()=>{
-    console.log(`server runing on port ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
